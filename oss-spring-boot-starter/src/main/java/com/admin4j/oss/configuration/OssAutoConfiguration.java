6dd0be4ff7e443b4c40e0450db80f8cf1f5c0dd2
andanyang <1218853253@qq.com>
2023-04-16 20:31:15 +0800

    AmazonS3Factory

--- oss-spring-boot-starter/src/main/java/com/admin4j/oss/configuration/OssAutoConfiguration.java
+++ oss-spring-boot-starter/src/main/java/com/admin4j/oss/configuration/OssAutoConfiguration.java
@@ -1,18 +1,12 @@
 package com.admin4j.oss.configuration;
 
+import com.admin4j.oss.AmazonS3Factory;
 import com.admin4j.oss.OssProperties;
 import com.admin4j.oss.OssTemplate;
 import com.admin4j.oss.UploadFileService;
 import com.admin4j.oss.impl.OssTemplateImpl;
 import com.admin4j.oss.impl.SimpleOSSUploadFileService;
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -32,27 +26,9 @@ import org.springframework.web.multipart.MultipartFile;
 public class OssAutoConfiguration {
 
     @Bean
-    @ConditionalOnMissingBean(AmazonS3.class)
-    public AmazonS3 ossClient(OssProperties ossProperties) {
-        // 客户端配置，主要是全局的配置信息
-        ClientConfiguration clientConfiguration = new ClientConfiguration();
-        clientConfiguration.setMaxConnections(ossProperties.getMaxConnections());
-        // url以及region配置
-        AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(
-                ossProperties.getEndpoint(), ossProperties.getRegion());
-        // 凭证配置
-        AWSCredentials awsCredentials = new BasicAWSCredentials(ossProperties.getAccessKey(),
-                ossProperties.getSecretKey());
-        AWSCredentialsProvider awsCredentialsProvider = new AWSStaticCredentialsProvider(awsCredentials);
-        // build amazonS3Client客户端
-        return AmazonS3Client.builder().withEndpointConfiguration(endpointConfiguration)
-                .withClientConfiguration(clientConfiguration).withCredentials(awsCredentialsProvider)
-                .disableChunkedEncoding().withPathStyleAccessEnabled(ossProperties.getPathStyleAccess()).build();
-    }
-
-    @Bean
-    @ConditionalOnBean(AmazonS3.class)
-    public OssTemplate ossTemplate(AmazonS3 amazonS3, OssProperties ossProperties) {
+    @ConditionalOnMissingBean(OssTemplate.class)
+    public OssTemplate ossTemplate(OssProperties ossProperties) {
+        AmazonS3 amazonS3 = AmazonS3Factory.create(ossProperties);
         return new OssTemplateImpl(amazonS3, ossProperties);
     }
 
