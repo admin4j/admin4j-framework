c8d29465ef0c49313514f6674f4a26de1f7f6c42
andanyang <1218853253@qq.com>
2023-07-03 17:18:37 +0800

    UserContextConfig

--- admin4j-common-spring-web/src/main/java/com/admin4j/common/config/UserContextConfig.java
+++ admin4j-common-spring-web/src/main/java/com/admin4j/common/config/UserContextConfig.java
@@ -2,25 +2,23 @@ package com.admin4j.common.config;
 
 import com.admin4j.common.service.IUserContextHolder;
 import com.admin4j.common.util.UserContextUtil;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author andanyang
  * @since 2023/6/7 8:54
  */
-public class UserContextConfig implements InitializingBean, ApplicationContextAware {
-    private ApplicationContext applicationContext;
+public class UserContextConfig implements InitializingBean {
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
+
+    @Autowired(required = false)
+    IUserContextHolder userContextHolder;
+
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        UserContextUtil.userContextHolder = applicationContext.getBean(IUserContextHolder.class);
+
+        UserContextUtil.userContextHolder = userContextHolder;
     }
 }
