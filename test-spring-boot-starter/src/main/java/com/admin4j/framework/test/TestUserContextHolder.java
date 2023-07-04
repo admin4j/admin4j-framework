12bb9f289f47c78df28279414c3e7a63b5022b6b
andanyang <1218853253@qq.com>
2023-07-04 08:58:04 +0800

    AuthenticationUser add OriginAuthentication

--- test-spring-boot-starter/src/main/java/com/admin4j/framework/test/TestUserContextHolder.java
+++ test-spring-boot-starter/src/main/java/com/admin4j/framework/test/TestUserContextHolder.java
@@ -64,7 +64,7 @@ public class TestUserContextHolder implements IUserContextHolder {
     public AuthenticationUser getLoginUser() {
         AuthenticationUser authenticationUser = THREAD_LOCAL_USER.get();
 
-        ResponseEnum.FAIL_UNAUTHORIZED.notNull(authenticationUser);
+        ResponseEnum.FAIL_AUTH_TOKEN.notNull(authenticationUser);
 
         return authenticationUser;
     }
