1e5efc2484882baed93cd9e1cbafc7e20c2d3e46
andanyang <1218853253@qq.com>
2023-07-05 17:33:14 +0800

    version pom

--- admin4j-common-spring-web/src/main/java/com/admin4j/common/util/ServletUtils.java
+++ admin4j-common-spring-web/src/main/java/com/admin4j/common/util/ServletUtils.java
@@ -1,7 +1,7 @@
 package com.admin4j.common.util;
 
 
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
