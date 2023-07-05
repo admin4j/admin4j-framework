9c97c050f76cc4e28037e2ec3a4adf4792ea302b
andanyang <1218853253@qq.com>
2023-07-05 13:14:24 +0800

    version

--- admin4j-lock/lock-core/src/main/java/com/admin4j/framework/lock/LockInfo.java
+++ admin4j-lock/lock-core/src/main/java/com/admin4j/framework/lock/LockInfo.java
@@ -1,6 +1,7 @@
 package com.admin4j.framework.lock;
 
 import com.admin4j.framework.lock.constant.LockModel;
+import com.admin4j.framework.lock.util.DistributedLockUtil;
 import lombok.Data;
 
 /**
@@ -38,4 +39,8 @@ public class LockInfo<T> {
      * 指定的执行器
      */
     private Class<? extends LockExecutor> executor;
+
+    public void setLockKey(String lockKey) {
+        this.lockKey = DistributedLockUtil.DISTRIBUTED_LOCK_PRE + lockKey;
+    }
 }
