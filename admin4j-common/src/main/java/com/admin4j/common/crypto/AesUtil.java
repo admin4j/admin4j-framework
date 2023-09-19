4a70c187d63f443d04f77f328aff85a9c72acb97
andanyang <1218853253@qq.com>
2023-09-19 17:11:40 +0800

    更新版本依赖

--- admin4j-common/src/main/java/com/admin4j/common/crypto/AesUtil.java
+++ admin4j-common/src/main/java/com/admin4j/common/crypto/AesUtil.java
@@ -36,19 +36,25 @@ public class AesUtil {
      *
      * @param data 需要加密的内容
      * @param key  加密密码
+     * @param iv   加密密码
      * @return
      */
-    public static byte[] encrypt(byte[] data, byte[] key) {
+    public static byte[] encrypt(byte[] data, byte[] key, byte[] iv) {
         try {
             if (key.length < OFFSET) {
                 byte[] tmp = new byte[OFFSET];
                 System.arraycopy(key, 0, tmp, 0, key.length);
                 key = tmp;
             }
+            if (iv.length < 16) {
+                byte[] tmp = new byte[16];
+                System.arraycopy(iv, 0, tmp, 0, iv.length);
+                iv = tmp;
+            }
             SecretKeySpec secretKey = new SecretKeySpec(key, ALGORITHM);
-            IvParameterSpec iv = new IvParameterSpec(key, 0, OFFSET);
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv, 0, OFFSET);
             Cipher cipher = Cipher.getInstance(AES_CBC_CIPHER);
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec);
             return cipher.doFinal(data);
         } catch (Exception e) {
             throw new EncryptException("aes encrypt error:" + e.getMessage(), e);
@@ -62,18 +68,22 @@ public class AesUtil {
      * @param key  解密密钥
      * @return
      */
-    public static byte[] decrypt(byte[] data, byte[] key) {
+    public static byte[] decrypt(byte[] data, byte[] key, byte[] iv) {
         try {
             if (key.length < 16) {
                 byte[] tmp = new byte[16];
                 System.arraycopy(key, 0, tmp, 0, key.length);
                 key = tmp;
             }
+            if (iv.length < 16) {
+                byte[] tmp = new byte[16];
+                System.arraycopy(iv, 0, tmp, 0, iv.length);
+                iv = tmp;
+            }
             SecretKeySpec secretKey = new SecretKeySpec(key, ALGORITHM);
-            byte[] enCodeFormat = secretKey.getEncoded();
-            SecretKeySpec secretKeySpec = new SecretKeySpec(enCodeFormat, ALGORITHM);
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv, 0, OFFSET);
             Cipher cipher = Cipher.getInstance(AES_CBC_CIPHER);
-            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(key));
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec);
             return cipher.doFinal(data);
         } catch (Exception e) {
             throw new EncryptException("ase decrypt error", e);
@@ -82,13 +92,21 @@ public class AesUtil {
 
     /**
      * 加密
+     * <p>
+     * * 定义 aes 加密的key
+     * * 密钥  必须是16位, 自定义,
+     * * 如果不是16位, 则会出现InvalidKeyException: Illegal key size
+     * * 解决方案有两种：
+     * * 需要安装Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files(可以在Oracle下载).
+     * * .设置设置key的长度为16个字母和数字的字符窜（128 Bit/8=16字符）就不报错了。
      *
      * @param data 需要加密的内容
      * @param key  加密密码
      * @return
      */
     public static String encrypt(String data, String key) {
-        byte[] valueByte = encrypt(data.getBytes(StandardCharsets.UTF_8), key.getBytes(StandardCharsets.UTF_8));
+        byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+        byte[] valueByte = encrypt(data.getBytes(StandardCharsets.UTF_8), keyBytes, keyBytes);
         return Base64.getEncoder().encodeToString(valueByte);
     }
 
@@ -101,7 +119,8 @@ public class AesUtil {
      */
     public static String decrypt(String data, String key) {
         byte[] originalData = Base64.getDecoder().decode(data.getBytes());
-        byte[] valueByte = decrypt(originalData, key.getBytes(StandardCharsets.UTF_8));
+        byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+        byte[] valueByte = decrypt(originalData, keyBytes, keyBytes);
         return new String(valueByte);
     }
 }
--- mybatis-plus-boot-starter/src/main/java/com/admin4j/framework/mp/type/AbstractEncryptTypeHandler.java
+++ mybatis-plus-boot-starter/src/main/java/com/admin4j/framework/mp/type/AbstractEncryptTypeHandler.java
@@ -9,6 +9,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
+ * 注意！！ 必须开启映射注解
+ * *
+ * * @TableName(autoResultMap = true)
+ * *
+ * * 以下两种类型处理器，二选一 也可以同时存在
+ * *
+ * * 注意！！选择对应的 JSON 处理器也必须存在对应 JSON 解析依赖包
+ * 使用xml是需要自己设置
+ * ```xml
+ * <resultMap id="myResultMap" type="com.example.MyObject">
+ * <result column="data" property="data" typeHandler="com.example.JacksonTypeHandler"/>
+ * </resultMap>
+ * ```
+ *
  * @author andanyang
  * @since 2023/9/19 9:06
  */
@@ -31,22 +45,22 @@ public abstract class AbstractEncryptTypeHandler<T> extends BaseTypeHandler<T> {
     protected abstract T decrypt(String ciphertext);
 
     @Override
-    public void setNonNullParameter(PreparedStatement preparedStatement, int i, T s, JdbcType jdbcType) throws SQLException {
-        preparedStatement.setString(i, encrypt(s));
+    public void setNonNullParameter(PreparedStatement preparedStatement, int i, T parameter, JdbcType jdbcType) throws SQLException {
+        preparedStatement.setString(i, encrypt(parameter));
     }
 
     @Override
-    public T getNullableResult(ResultSet resultSet, String s) throws SQLException {
-        return decrypt(resultSet.getString(s));
+    public T getNullableResult(ResultSet resultSet, String columnName) throws SQLException {
+        return decrypt(resultSet.getString(columnName));
     }
 
     @Override
-    public T getNullableResult(ResultSet resultSet, int i) throws SQLException {
-        return decrypt(resultSet.getString(1));
+    public T getNullableResult(ResultSet resultSet, int columnIndex) throws SQLException {
+        return decrypt(resultSet.getString(columnIndex));
     }
 
     @Override
-    public T getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
-        return decrypt(callableStatement.getString(1));
+    public T getNullableResult(CallableStatement callableStatement, int columnIndex) throws SQLException {
+        return decrypt(callableStatement.getString(columnIndex));
     }
 }
