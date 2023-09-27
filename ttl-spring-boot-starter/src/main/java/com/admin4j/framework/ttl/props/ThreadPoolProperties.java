1e0c124ac91e0a005ba30649415634d78333898f
andanyang <1218853253@qq.com>
2023-09-27 13:49:22 +0800

    threadNamePrefix

--- ttl-spring-boot-starter/src/main/java/com/admin4j/framework/ttl/props/ThreadPoolProperties.java
+++ ttl-spring-boot-starter/src/main/java/com/admin4j/framework/ttl/props/ThreadPoolProperties.java
@@ -1,5 +1,6 @@
 package com.admin4j.framework.ttl.props;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
@@ -7,6 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @since 2022/11/4 16:02
  */
 @ConfigurationProperties("admin4j.async")
+@Data
 public class ThreadPoolProperties {
     /**
      * 线程池维护线程的最小数量.
@@ -19,41 +21,9 @@ public class ThreadPoolProperties {
     /**
      * 队列最大长度
      */
-    private int queueCapacity = 10;
+    private int queueCapacity = 200;
     /**
      * 线程池前缀
      */
     private String threadNamePrefix = "Admin4jExecutor-";
-
-    public int getCorePoolSize() {
-        return corePoolSize;
-    }
-
-    public void setCorePoolSize(int corePoolSize) {
-        this.corePoolSize = corePoolSize;
-    }
-
-    public int getMaxPoolSize() {
-        return maxPoolSize;
-    }
-
-    public void setMaxPoolSize(int maxPoolSize) {
-        this.maxPoolSize = maxPoolSize;
-    }
-
-    public int getQueueCapacity() {
-        return queueCapacity;
-    }
-
-    public void setQueueCapacity(int queueCapacity) {
-        this.queueCapacity = queueCapacity;
-    }
-
-    public String getThreadNamePrefix() {
-        return threadNamePrefix;
-    }
-
-    public void setThreadNamePrefix(String threadNamePrefix) {
-        this.threadNamePrefix = threadNamePrefix;
-    }
 }
