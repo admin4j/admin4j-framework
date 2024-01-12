d40d0ae0bd17629d044c522608474f0410f44634
andanyang <1218853253@qq.com>
2024-01-12 10:01:50 +0800

    doc

--- web-spring-boot-starter/src/main/java/com/admin4j/framework/web/pojo/PageQuery.java
+++ web-spring-boot-starter/src/main/java/com/admin4j/framework/web/pojo/PageQuery.java
@@ -49,13 +49,18 @@ public class PageQuery implements Serializable {
     @Setter
     private boolean isAsc = false;
 
-    @ApiModelProperty(value = "是否需要分页", hidden = true)
+    @ApiModelProperty(value = "是否需要分页", hidden = true, accessMode = ApiModelProperty.AccessMode.READ_ONLY)
     private boolean needPage = true;
 
     @ApiModelProperty("是否显示分页")
     @Getter
     @Setter
     private boolean needCount = true;
+
+    public PageQuery(Integer pageNum, Integer pageSize) {
+        this.pageNum = pageNum;
+        this.pageSize = pageSize;
+    }
 
     /**
      * 获取默认的 排序字段
@@ -69,11 +74,6 @@ public class PageQuery implements Serializable {
 
     public String getOrderByColumn() {
         return StringUtils.defaultString(orderByColumn, getDefaultOrderByColumn());
-    }
-
-    public PageQuery(Integer pageNum, Integer pageSize) {
-        this.pageNum = pageNum;
-        this.pageSize = pageSize;
     }
 
     /**
