7436e5edfaf88381841b6857f7c75f86ba310632
andanyang <1218853253@qq.com>
2024-04-19 11:09:24 +0800

    feat(prometheus): 修改 spring.application.name 默认值

--- prometheus-spring-boot-starter/src/main/java/com/admin4j/framework/prometheus/config/PrometheusAutoConfiguration.java
+++ prometheus-spring-boot-starter/src/main/java/com/admin4j/framework/prometheus/config/PrometheusAutoConfiguration.java
@@ -3,14 +3,16 @@ package com.admin4j.framework.prometheus.config;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
 /**
  * @author andanyang
  * @since 2023/3/21 10:08
  */
+@ConditionalOnProperty(value = "spring.application.name")
 public class PrometheusAutoConfiguration {
-    @Value("${spring.application.name}")
+    @Value("${spring.application.name:''}")
     private String applicationName;
 
     @Bean
