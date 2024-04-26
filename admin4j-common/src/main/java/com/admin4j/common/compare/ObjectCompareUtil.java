b56a243e43900b88bea6667fac7c247098534eed
andanyang <1218853253@qq.com>
2024-04-26 18:16:23 +0800

    feat(ObjectCompareUtil): 添加字段比较接口 Comparator

--- admin4j-common/src/main/java/com/admin4j/common/compare/ObjectCompareUtil.java
+++ admin4j-common/src/main/java/com/admin4j/common/compare/ObjectCompareUtil.java
@@ -4,7 +4,9 @@ import com.admin4j.common.pojo.ResponseEnum;
 import org.apache.commons.lang3.ObjectUtils;
 
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
@@ -13,6 +15,17 @@ import java.util.Objects;
  * @since 2022/2/17 8:53
  */
 public class ObjectCompareUtil {
+
+    private static Comparator<Object> DEFAULT_FIELD_COMPARATOR;
+
+    {
+        DEFAULT_FIELD_COMPARATOR = (o1, o2) -> {
+            if (o1 instanceof BigDecimal) {
+                return ((BigDecimal) o1).compareTo((BigDecimal) o2);
+            }
+            return Objects.equals(o1, o2) ? 0 : 1;
+        };
+    }
 
     /**
      * 比较对象
@@ -23,6 +36,18 @@ public class ObjectCompareUtil {
      * @throws IllegalAccessException 当访问字段权限不足时抛出此异常
      */
     public static List<ObjectCompareFiledResult> compareFields(Object originObj, Object targetObj) throws IllegalAccessException {
+        return compareFields(originObj, targetObj, DEFAULT_FIELD_COMPARATOR);
+    }
+
+    /**
+     * 比较对象
+     *
+     * @param originObj 原始对象
+     * @param targetObj 目标对象
+     * @return 字段比较结果列表
+     * @throws IllegalAccessException 当访问字段权限不足时抛出此异常
+     */
+    public static List<ObjectCompareFiledResult> compareFields(Object originObj, Object targetObj, Comparator<Object> fieldComparator) throws IllegalAccessException {
         // 确保对象不为空
         ResponseEnum.VERIFY_ERROR.isTrue(ObjectUtils.allNotNull(originObj, targetObj), "obj is null");
 
@@ -64,7 +89,7 @@ public class ObjectCompareUtil {
             field.setAccessible(accessible);
 
             // 比较值
-            if (!Objects.equals(originValue, newValue)) {
+            if (fieldComparator.compare(originValue, newValue) == 0) {
                 ObjectCompareFiledResult objectCompareFiledResult = new ObjectCompareFiledResult();
 
                 objectCompareFiledResult.setField(field);
